55201a44962ebd54d0ae454fb9732af3118c81b6
NicolasGDM <34142471+NicolasGDM@users.noreply.github.com>
2019-07-08 14:42:59 +0200

    Create twitter_credentials.py

---
+++ twitter_credentials.py
@@ -0,0 +1,8 @@
+################################################################################
+##################### Fill up you Twitter Credentials ##########################
+ twitter_auth = {
+     'consumer_key': 'REPLACE_WITH_YOUR_CONSUMER_KEY',
+     'consumer_secret': 'REPLACE_WITH_YOUR_CONSUMER_SECRET_KEY',
+     'access_token': 'REPLACE_WITH_YOUR_ACCESS_TOKEN',
+     'access_token_secret': 'REPLACE_WITH_YOUR_ACCESS_SECRET_TOKEN',
+   }
